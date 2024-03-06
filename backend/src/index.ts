e21adfd8fda4c27f75db8b5c2b02f07bd0728cd6
Kanishk Singh <kanishk.singh198@gmail.com>
2024-03-06 13:37:54 +0530

    Feat: add rudimentary routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,29 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+  return c.text('signup route')
 })
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('signin route')
+})
+
+app.post('/api/v1/blog/', (c) => {
+  return c.text('post blog route')
+})
+
+app.put('/api/v1/blog/', (c) => {
+  return c.text('signin route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('get blog by id route')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('get all blogs route')
+})
+
 
 export default app
