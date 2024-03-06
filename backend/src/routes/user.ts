4f5bd6350de0147b04204f9bc1be34df7a6cbddc
Kanishk Singh <kanishk.singh198@gmail.com>
2024-03-07 02:16:05 +0530

    Feat: add input validation to auth routes from own npm package

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { sign } from "hono/jwt";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { signinInput, signupInput } from "@kanishk198/validator-common";
 
 const userRouter = new Hono<{
 	Bindings: {
@@ -16,6 +17,12 @@ userRouter.post("/signup", async (c) => {
 	}).$extends(withAccelerate());
 
 	const body = await c.req.json();
+	const { success } = signupInput.safeParse(body)
+	if (!success) {
+		c.status(411)
+		return c.json({message: "Invalid inputs."})
+	}
+
 	try {
 		const user = await prisma.user.create({
 			data: {
@@ -38,6 +45,13 @@ userRouter.post("/signin", async (c) => {
 	}).$extends(withAccelerate());
 
 	const body = await c.req.json();
+	const { success } = signinInput.safeParse(body)
+	console.log(success)
+	if (!success) {
+		c.status(411)
+		return c.json({message: "Invalid inputs."})
+	}
+
 	const user = await prisma.user.findUnique({
 		where: {
 			email: body.email,
