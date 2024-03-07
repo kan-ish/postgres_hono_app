3ce8652f7c477e9f0aac9dc1e0813bb38488a9ac
Kanishk Singh <kanishk.singh198@gmail.com>
2024-03-07 12:09:25 +0530

    Feat: add quote section to signup page

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,18 @@
+import { Quote } from "../components/Quote";
+
 export default function Signup() {
-    return (
-        <div>
-            Hello from Signup
-        </div>
-    )
+	return (
+		<div>
+			<div className="grid grid-cols-2">
+                <div>
+				    Hello from Signup
+
+                </div>
+
+                <div className="invisible lg:visible">
+                    <Quote />
+                </div>
+			</div>
+		</div>
+	);
 }
