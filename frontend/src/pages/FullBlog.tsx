2bbc53344e21cb0d5010299ec2f5acaf17e855de
Kanishk Singh <kanishk.singh198@gmail.com>
2024-03-11 17:50:31 +0530

    Fix: type warning on blog details page

--- frontend/src/pages/FullBlog.tsx
+++ frontend/src/pages/FullBlog.tsx
@@ -2,7 +2,7 @@ import { BlogType } from "../hooks";
 import { Appbar } from "../components/Appbar";
 import { Avatar } from "../components/Avatar";
 
-export const FullBlog = ({ blog }: BlogType) => {
+export const FullBlog = ({ blog }: {blog: BlogType}) => {
 	return (
 		<div>
 			<Appbar />
@@ -12,13 +12,17 @@ export const FullBlog = ({ blog }: BlogType) => {
 						<div className="text-5xl font-extrabold">
 							{blog.title || "Anonymous"}
 						</div>
+
 						<div className="text-slate-500 pt-2">
 							Post on 2nd December 2023
 						</div>
+
 						<div className="pt-4">{blog.content}</div>
 					</div>
+
 					<div className="col-span-4">
 						<div className="text-slate-600 text-lg">Author</div>
+					
 						<div className="flex w-full">
 							<div className="pr-4 flex flex-col justify-center">
 								<Avatar
@@ -26,10 +30,12 @@ export const FullBlog = ({ blog }: BlogType) => {
 									name={blog.author.name || "Anonymous"}
 								/>
 							</div>
+					
 							<div>
 								<div className="text-xl font-bold">
 									{blog.author.name || "Anonymous"}
 								</div>
+						
 								<div className="pt-2 text-slate-500">
 									Random catch phrase about the author's
 									ability to grab the user's attention
