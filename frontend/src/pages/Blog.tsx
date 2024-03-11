5a7a28956f7b11d21b43d23ddef3c0a4075a8e9f
Kanishk Singh <kanishk.singh198@gmail.com>
2024-03-11 17:40:39 +0530

    Feat: add blog details page to separate component

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,7 +1,19 @@
+import { useGetBlog } from "../hooks";
+import { useParams } from "react-router-dom";
+import { FullBlog } from "./FullBlog";
+
 export default function Blog() {
-    return (
-        <div>
-            Hello from Blog
-        </div>
-    )
+	const { id } = useParams();
+
+	const { loading, blog } = useGetBlog({ id: id || "" });
+
+	if (loading) {
+		return <div>loading...</div>;
+	}
+
+	return (
+		<div>
+			<FullBlog blog={blog} />
+		</div>
+	);
 }
