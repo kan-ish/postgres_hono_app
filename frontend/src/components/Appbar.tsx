575045c1f8817d23c78c40bf355506782f592a99
Kanishk Singh <kanishk.singh198@gmail.com>
2024-03-11 17:49:45 +0530

    Feat: add link to home on navbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 import { Avatar } from "./Avatar";
 
 export const Appbar = () => {
-    return(
-        <div className="border-b flex justify-between px-10 py-4">
-            <div className="flex flex-col justify-center">
-                Medium Rare
-            </div>
+	return (
+		<div className="border-b flex justify-between px-10 py-4">
+			<Link
+				to={"/blogs"}
+				className="flex flex-col justify-center cursor-pointer">
+				<div className="flex flex-col justify-center">Medium Rare</div>
+			</Link>
 
-            <div>
-                <Avatar name="Kanishk Singh" size="big" />
-            </div>
-        </div>
-    )
-}
+			<div>
+				<Avatar name="Kanishk Singh" size="big" />
+			</div>
+		</div>
+	);
+};
